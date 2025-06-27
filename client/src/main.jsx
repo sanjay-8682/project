343116b7d0d02c47f0b44ef05074fdf82e5a13b0
Sanjay G <sanjaysanjay123002@gmail.com>
2025-06-27 20:34:13 +0530

    changes to make main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-//import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
-import { persistor, store } from './redux/user/store.js'
+
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './redux/user/store.js';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-  <PersistGate persistor={persistor}>
-  <Provider loading={null} store={store}>
-    <App />
+ 
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
-    </PersistGate>
-  </BrowserRouter>,
-)
+  
+);
